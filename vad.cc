f8c27d36571f5b3fb95a5d14f7478b7a9a84f69d
song.yang <song.yang@ecovacs.com>
2017-11-30 14:52:09 +0800

    add window

--- vad.cc
+++ vad.cc
@@ -1,11 +1,8 @@
-// Created on 2017-06-03
-// Author: Binbin Zhang
-
 #include "vad.h"
 #include "stdio.h"
 #include "stdlib.h"
 
-Vad::Vad(int mode): mode_(mode) {
+Vad::Vad(int mode): mode_(mode), trigger_(false), window_len_(10), window_sum_(0) {
     if (WebRtcVad_Create(&handle_) < 0) {
         printf("Create webrtc vad handle error\n");
         exit(-1);
@@ -23,8 +20,8 @@ void Vad::SetMode(int mode) {
     WebRtcVad_set_mode(handle_, mode_);
 }
 
-bool Vad::IsSpeech(const int16_t *data, int num_samples, int fs) {
-    int ret = WebRtcVad_Process(handle_, fs, data, num_samples);
+bool Vad::IsSpeech(const int16_t *data, int num_point_per_frame, int sample_rate) {
+    int ret = WebRtcVad_Process(handle_, sample_rate, data, num_point_per_frame);
     switch (ret) {
         case 0:
             return false;
@@ -35,6 +32,38 @@ bool Vad::IsSpeech(const int16_t *data, int num_samples, int fs) {
             exit(-1);
             return false;
     }
+}
+
+char Vad::GetFrameState(const int16_t *data, int frame_len, int sample_rate) {
+    int num_point_per_frame = (int)(frame_len * sample_rate / 1000);
+    bool tag = IsSpeech(data, num_point_per_frame, sample_rate);
+    if (window_.size() < window_len_) {
+        window_sum_ += tag;
+        window_.push_back(tag);
+        return 'N';
+    } else {
+        window_sum_ += tag;
+        window_.push_back(tag);
+        while (window_.size() > window_len_) {
+            window_sum_ -= window_.at(0);
+            window_.pop_front();
+        }
+        if (!trigger_) {
+            if (window_sum_ >= 0.9 * window_len_) {
+                trigger_ = true;
+                return 'B';
+            } else {
+                return 'N';
+            }
+        } else {
+            if (window_sum_ <= 0.1 * window_len_) {
+                trigger_ = false;
+                return 'E';
+            } else {
+                return 'N';
+            }
+        }  
+    }      
 }
 
 
--- vad.h
+++ vad.h
@@ -1,11 +1,9 @@
-// Created on 2017-06-03
-// Author: Binbin Zhang
-
 #ifndef WEBRTC_VAD_H_
 #define WEBRTC_VAD_H_
 
 #include "webrtc/common_audio/vad/include/webrtc_vad.h"
 #include "webrtc/typedefs.h"
+#include <deque>
 
 // vad wrapper for webrtc vad
 
@@ -14,13 +12,18 @@ public:
     // @param mode, set its aggressiveness mode, which is an integer 
     //              between 0 and 3. 0 is the least aggressive about 
     //              filtering out non-speech, 3 is the most aggressive
-    Vad(int mode = 0); 
+    Vad(int mode = 3); 
     virtual ~Vad();
     void SetMode(int mode);
-    bool IsSpeech(const int16_t *data, int num_samples, int fs); 
+    bool IsSpeech(const int16_t *data, int num_point_per_frame, int sample_rate);
+    char GetFrameState(const int16_t *data, int frame_len, int sample_rate);
 private:
     int mode_;
     VadInst* handle_;
+    bool trigger_;
+    std::deque<int> window_;
+    int window_len_;
+    int window_sum_;
 };
 
 
